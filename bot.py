fb1ad14eac74b273d5d960ecb08beb2c8ecef14d
Florian Schmidt <flo.schmidt10@gmail.com>
2022-12-15 22:29:20 +0100

    mucho refactoring

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import logging
 import re
+import sqlite3
 from datetime import date, datetime, time, timedelta, timezone
 
 import scrapy
-import urllib3
 from pid import PidFile
 from pid.base import PidFileAlreadyLockedError
 from playwright.async_api import async_playwright
@@ -12,30 +12,130 @@ from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# the Mensa ID to crawl
-location = 140
+class JobManager:
+    con = sqlite3.connect("jobs.db")
+    cur = con.cursor()
+    loaded_jobs = dict()
 
-# job DB init
-import sqlite3
+    def load_jobs(self) -> None:
+        try:
+            data = JobManager.cur.execute("select * from chatids").fetchall()
+        except sqlite3.OperationalError as e:
+            logging.critical(f"sqlite3 is not properly set up. Exception: '{str(e)}'")
+            logging.critical("run DB_RESET.py to reset (will delete everything)")
+            exit()
 
-con = sqlite3.connect("jobs.db")
-cur = con.cursor()
+        for line in data:
+            try:
+                localDate = datetime.now().replace(hour=int(line[1]), minute=int(line[2]))
+                utcDate = localDate.astimezone(tz=timezone.utc)
+                utcTime = time(hour=utcDate.hour, minute=utcDate.minute)
+                
+                jobReference = application.job_queue.run_daily(callback=callback_heute, time=utcTime, chat_id=int(line[0]))
+                JobManager.loaded_jobs[int(line[0])] = jobReference
 
-# i'm as disappointed as you are.
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+            except Exception as e:
+                logging.critical(f"Failed to load jobs from sqlite3. Exception: {str(e)}")
+                logging.critical("Manually inspect table 'chatids' for malformed data")
+                exit()
+    
+    def add_job(self, id: int, hour: int, min: int) -> None:
+        JobManager.cur.execute("insert into chatids values(?,?,?)", [id, hour, min])
+        JobManager.con.commit()
 
-# logging format config
-logging.basicConfig(level=logging.WARN,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        # starting the job
+        localDate = datetime.now().replace(hour=int(hour), minute=int(min))
+        utcDate = localDate.astimezone(tz=timezone.utc)
+        utcTime = time(hour=utcDate.hour, minute=utcDate.minute)
+
+        jobReference = application.job_queue.run_daily(callback=callback_heute, time=utcTime, days=(1,2,3,4,5), chat_id=id)
+
+        # saving the ref to loaded job (so that it can be unloaded)
+        JobManager.loaded_jobs[id] = jobReference
+        def remove_job(self):
+            pass
+
+    def remove_job(self, id: int) -> None:
+        JobManager.cur.execute("delete from chatids where id = (?)", [id])
+        JobManager.con.commit()
+
+        JobManager.loaded_jobs[id].schedule_removal()
+        del JobManager.loaded_jobs[id]
+
+    def get_job_time(self, chat_id: int) -> tuple[str, str]:
+        time = JobManager.cur.execute("select hour,min from chatids where id=(?)", [chat_id]).fetchone()
+        return time
+
+def main():
+    # if pidfile exists ≙ program is already running: catch the pidfilelocked exc, exit()
+    try:
+        # prevents multiple instances of this script to run at the same time → easy way to restart in case of error
+        with PidFile():
+
+            # logging format config
+            logging.basicConfig(level=logging.WARN,
+                format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+            # loading API token for Telegram bot access
+            try:
+                with open("token.txt", "r") as fobj:
+                    token = fobj.readline().strip()
+            except FileNotFoundError:
+                logging.critical("'token.txt' missing. Create it and insert the token (without quotation marks)")
+                exit()
+
+            # :(
+            global application
+            application = ApplicationBuilder().token(token).read_timeout(30).write_timeout(30).connect_timeout(30).pool_timeout(30).build()
+            
+            # restoring all daily auto messages using chatids and times saved to jobs.db
+            job_manager = JobManager()
+            job_manager.load_jobs()
+            
+            start_handler = CommandHandler('start', start)
+            application.add_handler(start_handler)
+
+            heute_handler = CommandHandler('heute', heute)
+            application.add_handler(heute_handler)
+
+            morgen_handler = CommandHandler('morgen', morgen)
+            application.add_handler(morgen_handler)
+
+            uebermorgen_handler = CommandHandler('uebermorgen', uebermorgen)
+            ubermorgen_handler = CommandHandler('ubermorgen', uebermorgen)
+            application.add_handler(uebermorgen_handler)
+            application.add_handler(ubermorgen_handler)
+
+            subscribe_handler = CommandHandler('subscribe', subscribe)
+            application.add_handler(subscribe_handler)
+
+            unsubscribe_handler = CommandHandler('unsubscribe', unsubscribe)
+            application.add_handler(unsubscribe_handler)
+
+            changetime_handler = CommandHandler('changetime',  changetime)
+            application.add_handler(changetime_handler)
+
+            send_job_time_handler = CommandHandler('when',  send_job_time)
+            application.add_handler(send_job_time_handler)
+
+            force_get_new_grades_handler = CommandHandler('cd',  force_get_new_grades)
+            application.add_handler(force_get_new_grades_handler)
+
+            ack_handler = CommandHandler('ack', acknowledge)
+            application.add_handler(ack_handler)
+
+            application.job_queue.run_repeating(callback=job_get_new_grades, interval=300, chat_id=578278860)
+            
+            application.run_polling()
+
+    except PidFileAlreadyLockedError:
+        exit()
 
 
 
 
-loadedJobs = {}
-
-
-# required by Markdown V2
-def Markdown2Formatter(text):
+# escaping special Markdown V2 chars
+def Markdown2Formatter(text: str) -> str:
     text = text.replace(".", "\.")
     text = text.replace("!", "\!")
     text = text.replace("+", "\+")
@@ -48,68 +148,25 @@ def Markdown2Formatter(text):
     return text
 
 
-def registerJob(id, hour, min):
-
-    cur.execute("insert into chatids values(?,?,?)", [id, hour, min])
-    con.commit()
-
-
-    # starting the job
-    localDate = datetime.now().replace(hour=int(hour), minute=int(min))
-    utcDate = localDate.astimezone(tz=timezone.utc)
-    utcTime = time(hour=utcDate.hour, minute=utcDate.minute)
-
-    jobReference = application.job_queue.run_daily(callback=callback_heute, time=utcTime, days=(1,2,3,4,5), chat_id=id)
-
-    # saving the ref to loaded job (so that it can be unloaded)
-    loadedJobs[id] = jobReference
-
-    
-
-
-
-def unregisterJob(id):
-
-    cur.execute("delete from chatids where id = (?)", [id])
-    con.commit()
-
-    loadedJobs[id].schedule_removal()
-    loadedJobs.pop(id)
-
-
-
-
-
-
-def loadJobs():
-    try:
-        data = cur.execute("select * from chatids").fetchall()
-    except sqlite3.OperationalError as e:
-        logging.critical(f"sqlite3 is not properly set up. Exception: '{str(e)}'")
-        logging.critical("run DB_RESET.py to reset (will delete everything)")
-        exit()
-
-    for line in data:
-        try:
-
-            localDate = datetime.now().replace(hour=int(line[1]), minute=int(line[2]))
-            utcDate = localDate.astimezone(tz=timezone.utc)
-            utcTime = time(hour=utcDate.hour, minute=utcDate.minute)
-            
-            jobReference = application.job_queue.run_daily(callback=callback_heute, time=utcTime, chat_id=int(line[0]))
-            
-            loadedJobs[int(line[0])] = jobReference
-
-        except Exception as e:
-            logging.critical(f"Failed to load jobs from sqlite3. Exception: {str(e)}")
-            logging.critical("Manually inspect table 'chatids' for malformed data")
-
-
-def createMessageStringFromSpider(date, morgen=False):
+def createMessageStringFromSpider(date: datetime.date, morgen: bool = False):
 
     message = ""
-
     weekdays = ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]
+    
+    # the Mensa ID to crawl
+    mensen_ids = {
+        # "Alle Mensen": "all", ### currently unsupported
+        "Dittrichring": 153,
+        "Botanischen Garten": 127,
+        "Academica": 118,
+        "am Park": 106,
+        "Elsterbecken": 115,
+        "Medizincampus": 162,
+        "Peterssteinweg": 111,
+        "Schoenauer Str": 140,
+        "Tierklinik": 170
+        }
+    location = mensen_ids["Schoenauer Str"]
     
 
     # Heute ist Mo-Fr (fooden)
@@ -240,36 +297,24 @@ async def uebermorgen(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode=ParseMode.MARKDOWN_V2)
 
 
-async def dbg(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    day = int(context.args[0])
-    month = int(context.args[1])
-    year = int(context.args[2])
-    
-    dataDate = date.today().replace(year=year, month=month, day=day)
-
-    message = createMessageStringFromSpider(dataDate)
-    
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=message, parse_mode=ParseMode.MARKDOWN_V2)
-
-
-
 async def changetime(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
     chat_id=update.effective_chat.id
+    job_manager = JobManager()
 
-    current_time = cur.execute("select * from chatids where id=(?)",[chat_id]).fetchone()
-
+    current_time = job_manager.get_job_time(chat_id)
+    
     if current_time is None:
         message = "Automatische Nachrichten sind noch nicht aktiviert.\n/subscribe oder\n/subscribe \[Zeit] ausführen"
 
     elif len(context.args) != 0:
         try:
             hour, min = parseTime(context.args[0])
+            job_manager = JobManager()
 
-            unregisterJob(id=chat_id)
-            # adding chatid to database (so that job can be recreated @ server restart)
-            registerJob(id=chat_id, hour=hour, min=min)
+            # unload job and remove from DB
+            job_manager.remove_job(id=chat_id)
+            # creating and loading new job, and adding to DB
+            job_manager.add_job(id=chat_id, hour=hour, min=min)
 
             message = "Plan wird ab jetzt automatisch an Wochentagen "+ hour+":"+min + " Uhr gesendet."
 
@@ -286,19 +331,22 @@ async def changetime(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # confirmation message
     await context.bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
 
-async def gettime(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
+async def send_job_time(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id=update.effective_chat.id
+    job_manager = JobManager()
 
-    time = cur.execute("select hour,min from chatids where id=(?)", [chat_id]).fetchone()
-    message = str(time[0]) + ":" + str(time[1]) + " Uhr"
+    time = job_manager.get_job_time(chat_id)
+    if time is not None:
+        message = str(time[0]) + ":" + str(time[1]) + " Uhr"
+    else:
+        message = "Plan wird nicht automatisch gesendet"
 
     await context.bot.send_message(chat_id=chat_id, text=message, parse_mode=ParseMode.MARKDOWN)
 
 
 # checks time string for validity, and if successful, returns ints Hours, Mins
-def parseTime(strTime):
-    regex = "([01]?[0-9]|2[0-3]):[0-5][0-9]";
+def parseTime(strTime: str) -> tuple[int, int]:
+    regex = "([01]?[0-9]|2[0-3]):[0-5][0-9]"
     cregex = re.compile(regex)
 
     m = re.match(cregex, strTime)
@@ -324,10 +372,9 @@ async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
         hour, min = ("6", "00")
     
 
-
-    # adding chatid to database (so that job can be recreated @ server restart)
     try:
-        registerJob(chat_id, hour, min)
+        job_manager = JobManager()
+        job_manager.add_job(chat_id, hour, min)
         message = "Plan wird ab jetzt automatisch an Wochentagen "+ hour+":"+min + " Uhr gesendet.\n\n/changetime \[Zeit] zum Ändern\n/unsubscribe zum Deaktivieren"
 
     except sqlite3.IntegrityError:
@@ -340,12 +387,12 @@ async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
+async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id=update.effective_chat.id
 
     try:
-        unregisterJob(chat_id)
+        job_manager = JobManager()
+        job_manager.remove_job(chat_id)
         # confirmation message
         await context.bot.send_message(chat_id=chat_id, text="Plan wird nicht mehr automatisch gesendet.", parse_mode=ParseMode.MARKDOWN)
 
@@ -354,7 +401,7 @@ async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 # used as callback when called automatically (daily)
-async def callback_heute(context):
+async def callback_heute(context) -> None:
     job = context.job
     message = createMessageStringFromSpider(date.today())
 
@@ -368,10 +415,12 @@ async def playwright_fetch_grades() -> list:
     grades = list()
 
     async with async_playwright() as p:
+        # setting up browser
         browser = await p.chromium.launch(headless=True)
         context = await browser.new_context()
         page = await context.new_page()
 
+        # login page
         await page.goto("https://erp.campus-dual.de/sap/bc/webdynpro/sap/zba_initss?sap-client=100&sap-language=de&uri=https://selfservice.campus-dual.de/index/login")
         await page.get_by_role("textbox", name="Benutzer").click()
         await page.get_by_role("textbox", name="Benutzer").fill(uname)
@@ -379,14 +428,14 @@ async def playwright_fetch_grades() -> list:
         await page.get_by_role("textbox", name="Kennwort").fill(pw)
         await page.get_by_role("button", name="Anmelden").click()
 
+        # exam results page
         await page.goto("https://selfservice.campus-dual.de/acwork/index")
-
 
         table = page.locator("#acwork tbody")
         top_level_lines = table.locator(".child-of-node-0")
 
         count_top_level_lines = await top_level_lines.count()
-        # refs_all_children = []
+
 
         for i in range(count_top_level_lines):
             top_level_line = top_level_lines.nth(i)
@@ -406,12 +455,12 @@ async def playwright_fetch_grades() -> list:
         await browser.close()
         
         return grades
-    
 
 
+async def job_get_new_grades(context: ContextTypes.DEFAULT_TYPE):
+    if not context._chat_id == 578278860:
+        return
 
-async def getGrades(context: ContextTypes.DEFAULT_TYPE):
-    
     message = ""
     grades = await playwright_fetch_grades()
 
@@ -425,15 +474,16 @@ async def getGrades(context: ContextTypes.DEFAULT_TYPE):
         if grade not in acknowlegded:
             message += f"\n{grade[1]}:\n{grade[0]}\n"
 
-
     if message:
+        message = "Neue Ergebnisse:\n\n" + message
         await context.bot.send_message(chat_id=578278860, text=message, parse_mode=ParseMode.MARKDOWN)
 
-async def forceGetGrades(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    
+async def force_get_new_grades(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context._chat_id == 578278860:
+        return
+
     message = ""
     grades = await playwright_fetch_grades()
-
 
     for grade in grades:
         message += f"\n{grade[0]}\n{grade[1]}\n{grade[2]}\n"
@@ -442,9 +492,12 @@ async def forceGetGrades(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def acknowledge(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context._chat_id == 578278860:
+        return
+
     message = ""
 
-    if len(context.args) == 0:
+    if not context.args:
         # get currently acknowledged
         with open('acknowledged.txt', 'r') as fobj:
             linecount = int()
@@ -474,64 +527,4 @@ async def acknowledge(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == '__main__':
-
-    # if pidfile exists ≙ program is already running: catch the pidfilelocked exc, exit()vi
-    try:
-        # prevents multiple instances of this script to run at the same time → easy way to restart in case of error
-        with PidFile():
-
-            try:
-                with open("token.txt", "r") as fobj:
-                    token = fobj.readline().strip()
-            except FileNotFoundError:
-                logging.critical("'token.txt' missing. Create it and insert the token (without quotation marks)")
-                exit()
-
-
-            application = ApplicationBuilder().token(token).read_timeout(30).write_timeout(30).connect_timeout(30).pool_timeout(30).build()
-            
-            # restoring all daily auto messages using chatids and times saved to jobs.db
-            loadJobs()
-
-            dbg_handler = CommandHandler('dbg', dbg)
-            application.add_handler(dbg_handler)
-            
-            start_handler = CommandHandler('start', start)
-            application.add_handler(start_handler)
-
-            heute_handler = CommandHandler('heute', heute)
-            application.add_handler(heute_handler)
-
-            morgen_handler = CommandHandler('morgen', morgen)
-            application.add_handler(morgen_handler)
-
-            uebermorgen_handler = CommandHandler('uebermorgen', uebermorgen)
-            application.add_handler(uebermorgen_handler)
-
-            ubermorgen_handler = CommandHandler('ubermorgen', uebermorgen)
-            application.add_handler(ubermorgen_handler)
-
-            subscribe_handler = CommandHandler('subscribe', subscribe)
-            application.add_handler(subscribe_handler)
-
-            unsubscribe_handler = CommandHandler('unsubscribe', unsubscribe)
-            application.add_handler(unsubscribe_handler)
-
-            changetime_handler = CommandHandler('changetime',  changetime)
-            application.add_handler(changetime_handler)
-
-            gettime_handler = CommandHandler('time',  gettime)
-            application.add_handler(gettime_handler)
-
-            forceGetGrades_handler = CommandHandler('cd',  forceGetGrades)
-            application.add_handler(forceGetGrades_handler)
-
-            ack_handler = CommandHandler('ack', acknowledge)
-            application.add_handler(ack_handler)
-
-            application.job_queue.run_repeating(callback=getGrades, interval=300, chat_id=578278860)
-            
-            application.run_polling()
-
-    except PidFileAlreadyLockedError:
-        exit()
+    main()
